52ab0904b590c9138fef17fb96c87241a8eda1d8
Yusin <34501891+yusin0903@users.noreply.github.com>
2024-01-20 02:57:20 +0800

    Update urls.py

--- DRF_api/urls.py
+++ DRF_api/urls.py
@@ -3,9 +3,6 @@ from DRF_api import views
 from DRF_api import views as user_view
 # define the urls
 urlpatterns = [
-    # path('tasks/', views.tasks),
-    # path('tasks/<int:pk>/', views.task_detail),
-    path('users/', user_view.UsersView.as_view()),
     path('users/<str:user>', user_view.DeleteView.as_view()),
     path('user/uploadcsv', user_view.MyFileView.as_view()),
     path('user/groupby', user_view.GroupView.as_view()),
